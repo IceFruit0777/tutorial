4a3f1e070d531c7c074a16773a41540570a92da4
IceFruit <13477035189@163.com>
2024-12-11 15:30:10 +0800

    生产环境部署准备

--- src/config.rs
+++ src/config.rs
@@ -1,11 +1,19 @@
+use std::time::Duration;
+
 use actix_web::web;
 use secrecy::{ExposeSecret, SecretString};
-use sqlx::PgPool;
+use sqlx::{postgres::PgPoolOptions, PgPool};
 
 #[derive(serde::Deserialize)]
 pub struct Config {
     pub database: DBConfig,
-    pub web_port: u16,
+    pub app: App,
+}
+
+#[derive(serde::Deserialize)]
+pub struct App {
+    pub host: String,
+    pub port: u16,
 }
 
 #[derive(serde::Deserialize)]
@@ -29,20 +37,69 @@ impl DBConfig {
         )
     }
 
-    pub async fn connect(&self) -> web::Data<PgPool> {
+    pub fn connect_str_without_db(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}",
+            self.username,
+            self.password.expose_secret(),
+            self.host,
+            self.port
+        )
+    }
+
+    pub fn connect(&self) -> web::Data<PgPool> {
         web::Data::new(
-            PgPool::connect(&self.connect_str())
-                .await
-                .expect("failed connect to postgres."),
+            PgPoolOptions::new()
+                .acquire_timeout(Duration::from_secs(5))
+                .connect_lazy(&self.connect_str())
+                .expect("failed to create postgres connection pool."),
         )
     }
 }
 
+enum Enviroment {
+    Local,
+    Production,
+}
+
+impl Enviroment {
+    fn as_str(&self) -> &str {
+        match self {
+            Enviroment::Local => "local",
+            Enviroment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Enviroment {
+    type Error = String;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{other} is not a valid enviroment. \
+                use `local` or `production` instead."
+            )),
+        }
+    }
+}
+
 pub fn get_config() -> Config {
+    let base_path = std::env::current_dir().expect("failed to determine current directory.");
+    let config_dir = base_path.join("config");
+    let env: Enviroment = std::env::var("APP_ENVIROMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("failed to parse APP_ENVIROMENT");
+    let env_filename = format!("{}.yaml", env.as_str());
+
     config::Config::builder()
-        .add_source(config::File::new("config.yaml", config::FileFormat::Yaml))
+        .add_source(config::File::from(config_dir.join("base.yaml")))
+        .add_source(config::File::from(config_dir.join(env_filename)))
         .build()
-        .expect("failed to read config.yaml.")
+        .expect("failed to read config.")
         .try_deserialize::<Config>()
-        .expect("failed to deserialize config.yaml.")
+        .expect("failed to deserialize config.")
 }
